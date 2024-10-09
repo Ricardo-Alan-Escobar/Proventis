7e4eb2ed53cdbe57e5974ef5d930ebf2a8f3b303
Ricardo Alan Escobar <ricardoalangomezescobar@gmail.com>
2024-10-09 09:33:07 -0600

    nueva rama

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -109,8 +109,9 @@ export default function Authenticated({ header, children }) {
                                             method="post"
                                             as="button"
                                         >
-                                            Cerrar Sesión
-                                        </Dropdown.Link>
+                                            <div className="flex">
+                                           Cerrar Sesión<svg xmlns="http://www.w3.org/2000/svg" width="14" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-log-out ml-2"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/><polyline points="16 17 21 12 16 7"/><line x1="21" x2="9" y1="12" y2="12"/></svg>
+                                        </div></Dropdown.Link>
                                     </Dropdown.Content>
                                 </Dropdown>
                             </div>
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -8,7 +8,7 @@ import { createRoot, hydrateRoot } from 'react-dom/client';
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => `${title} - Proventis`,
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.jsx`,
