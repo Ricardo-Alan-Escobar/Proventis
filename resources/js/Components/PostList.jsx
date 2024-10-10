11a6ea9aa22ade3df68989ab6c9a052303cb1f4b
Ricardo Alan Escobar <ricardoalangomezescobar@gmail.com>
2024-10-10 16:37:37 -0600

    Cambios en hora del post

--- resources/js/Components/PostList.jsx
+++ resources/js/Components/PostList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 
 export default function PostList({ posts }) {
     return (
@@ -7,7 +8,9 @@ export default function PostList({ posts }) {
                 <div key={post.id} className="p-4 mb-4 bg-white rounded shadow-sm">
                     <div className="flex justify-between items-center mb-2">
                         <div className="text-sm text-gray-600">{post.user.name}</div>
-                        <div className="text-xs text-gray-500">{post.created_at}</div>
+                        <div className="text-xs text-gray-500">
+                            {moment(post.created_at).format('MMMM Do YYYY, h:mm:ss a')}
+                        </div>
                     </div>
                     <div className="mb-2 text-gray-900">{post.content}</div>
                     {post.image && (
