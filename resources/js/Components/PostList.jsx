73a83742d942de4bca1f16ca0f60ae80498d913c
Ricardo Alan Escobar <ricardoalangomezescobar@gmail.com>
2024-10-14 10:06:08 -0600

    Post actualizado

--- resources/js/Components/PostList.jsx
+++ resources/js/Components/PostList.jsx
@@ -7,6 +7,7 @@ export default function PostList({ posts }) {
             {posts.map((post) => (
                 <div key={post.id} className="p-4 mb-4 bg-white rounded-md shadow-sm">
                     <div className="flex justify-between items-center mb-2">
+                        
                         <div className="text-sm text-gray-600">{post.user.name}</div>
                         <div className="text-xs text-gray-500">
                             {moment(post.created_at).format('MMMM Do YYYY, h:mm:ss a')}
