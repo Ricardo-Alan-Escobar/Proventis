33003b189c53a3e6d70ffbf4344c9a8d3bef0dea
Ricardo Alan Escobar <ricardoalangomezescobar@gmail.com>
2024-10-17 16:28:41 -0600

    Contenido del post modificado

--- resources/js/Components/PostList.jsx
+++ resources/js/Components/PostList.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import moment from 'moment';
-import { Pencil, Trash2, CircleChevronDown } from "lucide-react"
+import { Pencil, Trash2, CircleChevronDown } from "lucide-react";
+import Modal from './Modal'; // Asegúrate de que el componente Modal esté importado
 
 export default function PostList({ posts }) {
     const [editPostId, setEditPostId] = useState(null);
     const [editContent, setEditContent] = useState('');
     const [showMenuId, setShowMenuId] = useState(null); 
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [postToDelete, setPostToDelete] = useState(null);
+    const [expandedPosts, setExpandedPosts] = useState({}); // Estado para las publicaciones expandidas
 
     const handleEdit = (post) => {
         setEditPostId(post.id);
@@ -18,12 +22,22 @@ export default function PostList({ posts }) {
         setEditPostId(null); 
     };
 
-    const handleDelete = async (postId) => {
+    const openModal = (postId) => {
+        setPostToDelete(postId);
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+        setPostToDelete(null);
+    };
+
+    const confirmDelete = async () => {
         try {
-            if (confirm('¿Estas seguro de querer eliminar la publicación?')) {
-                await axios.delete(`/posts/${postId}`);
-                window.location.reload();
-            }
+            await axios.delete(`/posts/${postToDelete}`);
+            setIsModalOpen(false);
+            setPostToDelete(null);
+            window.location.reload();
         } catch (error) {
             console.error(error);
         }
@@ -40,21 +54,39 @@ export default function PostList({ posts }) {
     };
 
     const toggleMenu = (postId) => {
-        // Si el menú ya está abierto, lo oculta; si no, lo muestra
         if (showMenuId === postId) {
-            setShowMenuId(null); // Ocultar el menú
+            setShowMenuId(null); 
         } else {
-            setShowMenuId(postId); // Mostrar el menú
+            setShowMenuId(postId); 
         }
     };
 
-    const processContent = (content) => {
-        // Reemplazar '#' con etiquetas de título y '*' para negritas, respetar saltos de línea
-        const processedContent = content
-            .replace(/##(.+?)##/g, '<p class="text-lg font-bold">$1</p>') // Títulos h3 con ##
-            .replace(/#(.+?)#/g, '<p class="text-2xl font-bold">$1</p>')   // Títulos h2 con #
-            .replace(/\*(.+?)\*/g, '<strong>$1</strong>')                        // Texto en negritas con *
-            .replace(/\n/g, '<br />');                                           // Saltos de línea
+    // Función para recortar texto si es más largo que el límite
+    const toggleExpand = (postId) => {
+        setExpandedPosts(prev => ({
+            ...prev,
+            [postId]: !prev[postId] // Alterna entre expandido y contraído
+        }));
+    };
+
+    const isExpanded = (postId) => expandedPosts[postId];
+
+    const truncateText = (text, postId, limit = 500) => {
+        if (text.length <= limit) {
+            return text;
+        }
+
+        return isExpanded(postId)
+            ? text // Si está expandido, muestra todo el texto
+            : text.slice(0, limit) + '...'; // Si no, muestra solo el límite
+    };
+
+    const processContent = (content, postId) => {
+        const processedContent = truncateText(content, postId)
+            .replace(/##(.+?)##/g, '<p class="text-lg font-bold">$1</p>')
+            .replace(/#(.+?)#/g, '<p class="text-2xl font-bold">$1</p>')
+            .replace(/\*(.+?)\*/g, '<strong>$1</strong>')
+            .replace(/\n/g, '<br />');
 
         return { __html: processedContent };
     };
@@ -62,22 +94,21 @@ export default function PostList({ posts }) {
     return (
         <div>
             {posts.map((post) => (
-                <div key={post.id} className="p-6 mb-3 bg-white rounded-md shadow-md relative ">
+                <div key={post.id} className="p-6 mb-3 bg-white rounded-md shadow-md relative">
                     <div className="flex justify-between items-center mb-4">
                         <div className="text-sm text-gray-600">{post.user.name}</div>
                         <div className="text-xs text-gray-500">
                             {moment(post.created_at).format('MMMM Do YYYY, h:mm:ss a')}
                         </div>
 
-                        {/* Menú desplegable para editar y eliminar */}
                         <div className="relative">
                             <button
                                 className="text-gray-500 hover:text-gray-700"
-                                onClick={() => toggleMenu(post.id)} // Alternar el menú al hacer clic
+                                onClick={() => toggleMenu(post.id)}
                             >
                                <CircleChevronDown />
                             </button>
-                            {showMenuId === post.id && ( // Mostrar el menú solo para la publicación actual
+                            {showMenuId === post.id && (
                                 <div className="absolute right-0 mt-2 w-10 bg-white border border-gray-300 rounded-md shadow-lg z-10">
                                     <button
                                         onClick={() => handleEdit(post)}
@@ -86,7 +117,7 @@ export default function PostList({ posts }) {
                                         <Pencil /> 
                                     </button>
                                     <button
-                                        onClick={() => handleDelete(post.id)}
+                                        onClick={() => openModal(post.id)}
                                         className="block w-full px-2 py-2 text-left text-red-500 hover:bg-gray-200"
                                     >
                                         <Trash2 />
@@ -96,7 +127,6 @@ export default function PostList({ posts }) {
                         </div>
                     </div>
 
-                    {/* Contenido o edición en línea */}
                     {editPostId === post.id ? (
                         <div>
                             <textarea
@@ -112,7 +142,7 @@ export default function PostList({ posts }) {
                                     Guardar
                                 </button>
                                 <button
-                                    onClick={handleCancelEdit} // Botón para cancelar la edición
+                                    onClick={handleCancelEdit}
                                     className="px-4 py-2 bg-gray-500 text-white rounded-md hover:bg-gray-600"
                                 >
                                     Cancelar
@@ -120,10 +150,18 @@ export default function PostList({ posts }) {
                             </div>
                         </div>
                     ) : (
-                        <div
-                            className="mb-2 text-gray-900"
-                            dangerouslySetInnerHTML={processContent(post.content)} // Renderizar contenido con títulos, negritas y saltos de línea procesados
-                        />
+                        <div className="mb-2 text-gray-900">
+                            <div dangerouslySetInnerHTML={processContent(post.content, post.id)} />
+                            {/* Botón para expandir/contraer texto */}
+                            {post.content.length > 100 && (
+                                <button
+                                    onClick={() => toggleExpand(post.id)}
+                                    className="text-blue-500 hover:underline"
+                                >
+                                    {isExpanded(post.id) ? 'Ver menos' : 'Ver más'}
+                                </button>
+                            )}
+                        </div>
                     )}
 
                     {post.image && (
@@ -143,6 +181,29 @@ export default function PostList({ posts }) {
                     )}
                 </div>
             ))}
+
+            {isModalOpen && (
+                <Modal onClose={closeModal}>
+                    <div className="p-4">
+                        <h2 className="text-xl font-bold mb-4">Confirmar eliminación</h2>
+                        <p>¿Estás seguro de que deseas eliminar esta publicación?</p>
+                        <div className="flex justify-end mt-4">
+                            <button
+                                onClick={closeModal}
+                                className="mr-2 px-4 py-2 bg-gray-500 text-white rounded-md hover:bg-gray-600"
+                            >
+                                Cancelar
+                            </button>
+                            <button
+                                onClick={confirmDelete}
+                                className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600"
+                            >
+                                Eliminar
+                            </button>
+                        </div>
+                    </div>
+                </Modal>
+            )}
         </div>
     );
 }
