916e02ae24e32dafb93c6cf69ead1e6e9bf0654a
Ricardo Alan Escobar <ricardoalangomezescobar@gmail.com>
2024-10-18 13:52:59 -0600

    Modificacion en la vista de configuracion de la cuenta

--- resources/js/Pages/Profile/Edit.jsx
+++ resources/js/Pages/Profile/Edit.jsx
@@ -29,9 +29,7 @@ export default function Edit({ mustVerifyEmail, status }) {
                         <UpdatePasswordForm className="max-w-xl" />
                     </div>
 
-                    <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8">
-                        <DeleteUserForm className="max-w-xl" />
-                    </div>
+                    
                 </div>
             </div>
         </AuthenticatedLayout>
