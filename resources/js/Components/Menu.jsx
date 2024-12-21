d8bda0a66aeeca6dc7e15c6aae50f3cab01088d7
Ricardo Alan Escobar <ricardoalangomezescobar@gmail.com>
2024-12-21 13:24:07 -0600

    Cambions en inicio

--- resources/js/Components/Menu.jsx
+++ resources/js/Components/Menu.jsx
@@ -1,4 +1,4 @@
-import { Home, Ticket, BookOpen, Palmtree, FileText } from "lucide-react"
+import { Home, Ticket, BookOpen, Palmtree, FileText, Logs } from "lucide-react"
 import React from "react"
 import NavLink from "./NavLinkmenu"
 
@@ -6,8 +6,8 @@ export default function Menu() {
   return (
 
     <nav className="flex flex-col justify-center bg-white h-auto py-5 w-full shadow-md rounded-lg">
-        <h1 className="text-2xl ml-6  mb-5 font-bold">Menu</h1>
-
+        <h1 className="text-2xl   mb-5 font-bold flex items-center justify-between px-6">Menu <Logs /> </h1>
+<hr />
       <NavLink href={route('dashboard')} className="flex items-center space-x-3 px-4 " active={route().current('dashboard')}>
                 <Home className="w-5 h-5" />
                 <span className="text-sm font-medium">Inicio</span>
